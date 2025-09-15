4a78c40bcdcb34c2332fd7e6b495f61129f82d65
Abhinav339712 <128727420+Abhinav339712@users.noreply.github.com>
2025-09-15 13:29:42 +0530

    Add files via upload

--- tb/seq_detector_tb.v
+++ tb/seq_detector_tb.v
@@ -1,1 +1,96 @@
+module seq_detector_tb();
+reg clk,rst,seq_in;
+wire seq_out;
+
+parameter CYCLE = 10;
+
+seq_detector SED(.clk(clk),.rst(rst),.seq_in(seq_in),.seq_out(seq_out));
+
+always
+begin
+	#(CYCLE/2)
+	clk = 1'b0;
+	#(CYCLE/2)
+	clk = ~clk;
+end
+
+task initialize();
+	begin
+		seq_in = 1'b0;
+	end
+endtask
+
+task reset();
+	begin
+		@(negedge clk)
+		rst = 1'b1;
+		@(negedge clk)
+		rst = 1'b0;
+	end
+endtask
+
+task stimulus(input i);
+	begin
+	   @(negedge clk)
+		seq_in = i;
+	end
+endtask
+
+initial
+begin
+	reset();
+	stimulus(1);
+	stimulus(0);
+	stimulus(1);
+	stimulus(1);
+	stimulus(0);
+	reset();
+	stimulus(0);
+	stimulus(1);
+	stimulus(0);
+	stimulus(1);
+	stimulus(1);
+	stimulus(0);
+	#10;
+end
+
+initial $monitor("seq_in = %b,seq_out = %b",seq_in,seq_out);
+
+initial #400 $finish;
+
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
